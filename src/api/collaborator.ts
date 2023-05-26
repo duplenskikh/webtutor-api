7cb9ee06056a89c2df90444a39eeefc16f5407b5
umbrik <idumbrik@gmail.com>
2023-05-26 21:08:25 +0300

    Updated collaborator endpoint

--- src/api/collaborator.ts
+++ src/api/collaborator.ts
@@ -7,9 +7,29 @@ export function functions(): Route[] {
     pattern: "/collaborator",
     callback: "getCollaborator",
     access: "user"
+  }, {
+    method: "GET",
+    pattern: "/collaborators",
+    callback: "getCollaborators",
+    access: "user"
   }];
 }
 
 export function getCollaborator(params: HandlerParams, Request: Request) {
   return dapi.utils.response.ok(`curUserID is ${Request.Session.Env.curUserID}`);
+}
+
+export function getCollaborators(params: HandlerParams, Request: Request) {
+  const query = ArraySelectAll(tools.xquery(`for $e in collaborators return $e`));
+  const result = [];
+
+  for (let i = 0; i < query.length; i++) {
+    result.push({
+      id: query[i].id.Value,
+      fullname: query[i].fullname.Value,
+      sex: query[i].sex.Value
+    });
+  }
+
+  return dapi.utils.response.ok(result);
 }
