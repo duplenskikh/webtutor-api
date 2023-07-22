dfe55fd71c88f38d972be728b07d2e38829446e1
umbrik <idumbrik@gmail.com>
2023-07-22 09:18:49 +0300

    Добавлены логи для наблюдателя

--- src/controllers/deployer.ts
+++ src/controllers/deployer.ts
@@ -29,19 +29,21 @@ export function deploy(params: HandlerParams,req: Request) {
   const newContent = req.Body;
 
   if (StrLen(newContent) === 0) {
+    dapi.utils.log.warning(`Невозможно обновить. Файл ${fileUrl} не содержит контента`, "deployer");
     return dapi.utils.response.abort("Empty file content", 400);
   }
   
   const newHash = Md5Hex(newContent);
 
   if (oldHash == newHash) {
+    dapi.utils.log.warning(`Невозможно обновить. Хэши предыдущей и текущей версии файла ${fileUrl} сопадают`, "deployer");
     return dapi.utils.response.ok(null, 304);
   };
 
   ObtainDirectory(ParentDirectory(UrlToFilePath(fileUrl)), true);
   PutFileData(UrlToFilePath(fileUrl), newContent);
-
   DropFormsCache(fileUrl);
+  dapi.utils.log.info(`Файл ${fileUrl} обновлен и сброшен кэш`, "deployer");
   dapi.init();
 
   return dapi.utils.response.ok(fileUrl);
